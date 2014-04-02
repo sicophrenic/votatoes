defa021bd938e7d77e740924b44d11c4f74c6f59
Benjamin Hsieh <bhsiehh@gmail.com>
2014-04-02 16:38:22 -0700

    Revise and refactor
    Update README with a development note
    Reset CSS files with a reset file and comment out existing styles
    Modify original routes to be more concise
    Edit footer to be more conditional

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Votatoes
+
+## Misc Notes
+* ```:obj_id``` is necessary to store distinction between TVDB and TMDB objects
--- config/routes.rb
+++ config/routes.rb
@@ -1,79 +1,28 @@
-#-*- coding: utf-8 -*-#
 Votatoes::Application.routes.draw do
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
   root 'pages#root'
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
   get "pages/home"
   get 'pages/about', :as => 'about'
-  get 'votatos/find', :as => 'find_votato'
-  get 'votatos/search', :as => 'search_votato'
 
-  get 'votatos/new/:obj_id/tvdb' => 'votatos#new_tvdb_votato', :as => 'new_tvdb_votato'
-  post 'votatos/create/:obj_id/tvdb' => 'votatos#create_tvdb_votato', :as => 'create_tvdb_votato'
-  get 'votatos/new/:obj_id/tmdb' => 'votatos#new_tmdb_votato', :as => 'new_tmdb_votato'
-  post 'votatos/create/:obj_id/tmdb' => 'votatos#create_tmdb_votato', :as => 'create_tmdb_votato'
+  scope 'votatos', :controller => 'votatos' do
+    get 'find', :as => 'find_votato'
+    get 'search', :as => 'search_votato'
+    get ':obj_id/tvdb' => 'votatos#new_tvdb_votato', :as => 'new_tvdb_votato'
+    get ':obj_id/tmdb' => 'votatos#new_tmdb_votato', :as => 'new_tmdb_votato'
 
-  delete 'votatos/:id' => 'votatos#destroy', :as => 'destroy_votato'
-  delete 'plantations/:id' => 'plantations#destroy', :as => 'destroy_plantation'
+    post 'query', :as => 'query_votato'
+    post ':obj_id/tvdb' => 'votatos#create_tvdb_votato', :as => 'create_tvdb_votato'
+    post ':obj_id/tmdb' => 'votatos#create_tmdb_votato', :as => 'create_tmdb_votato'
+    post ':id/plant' => 'votatos#plan', :as => 'plant'
+    post 'votatos/:id/pluck' => 'votatos#pluck', :as => 'pluck'
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-  post 'votatos/:id/plant' => 'votatos#plant', :as => 'plant'
-  post 'votatos/:id/pluck' => 'votatos#pluck', :as => 'pluck'
-  post 'query' => 'votatos#query'
+    delete ':id' => 'votatos#destroy', :as => 'destroy_votato'
+  end
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
   resources :plantations
   # resources :votatos - Added by default
 
   # Devise
   devise_for :users
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
